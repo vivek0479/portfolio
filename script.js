1b769fa61a16ff49a440609e4ad1a44b34a1caf5
vivekananda <vivekanandabheemankar0@gmail.com>
2026-02-19 22:24:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -33,3 +33,19 @@ const observer = new IntersectionObserver(
 );
 
 reveals.forEach(el => observer.observe(el));
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navLinks.classList.toggle("active");
+});
+
+/* Close menu when link clicked */
+document.querySelectorAll(".nav-links a").forEach(link => {
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navLinks.classList.remove("active");
+  });
+});
+
