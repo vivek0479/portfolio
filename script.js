0fdfc2234defd631a2c402d8f2ccc70180bc2855
vivekananda <vivekanandabheemankar0@gmail.com>
2026-02-19 21:48:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const hero = document.querySelector(".hero-content");
+
+window.addEventListener("scroll", () => {
+  const scrollY = window.scrollY;
+  hero.style.transform = `translateY(${scrollY * 0.2}px)`;
+  hero.style.opacity = 1 - scrollY / 600;
+});
+const words = [
+  "VIVEKANANDA",
+  "DEVELOPER",
+  "CREATOR",
+  "ECE ENGINEER"
+];
+
+let index = 0;
+const textElement = document.getElementById("changing-text");
+
+setInterval(() => {
+  index = (index + 1) % words.length;
+  textElement.textContent = words[index];
+}, 2500);
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("active");
+      }
+    });
+  },
+  { threshold: 0.2 }
+);
+
+reveals.forEach(el => observer.observe(el));
